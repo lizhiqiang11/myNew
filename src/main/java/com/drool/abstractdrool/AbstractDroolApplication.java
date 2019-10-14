0e78a101893004e3ab58d0e862c22fe136f9c84e
lizhiqiang <zhiqiang.li@hexinkang.com>
2019-10-14 16:33:57 +0800

    my test

--- src/main/java/com/drool/abstractdrool/AbstractDroolApplication.java
+++ src/main/java/com/drool/abstractdrool/AbstractDroolApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AbstractDroolApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(AbstractDroolApplication.class, args);
     }
